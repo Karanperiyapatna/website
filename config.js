cfbf55d619ea315cfcce487a2638764aaf691aa1
Karan <periyapatnakaran@gmail.com>
2026-03-19 06:27:18 +0530

    support page changed to contact

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const CONFIG = {
-  JOB_URL: "hire-job.html",
-  AGENT_URL: "agent-registration.html"
+    JOB_URL: "hire-job.html",
+    AGENT_URL: "agent-registration.html",
+    SUPPORT_EMAIL: "support@digilaboursolutions.com",
+    SUPPORT_CONTACT_NO: "9986909171"
 };

3ccf8fdca0d9c4f7f191914283cdae9a5bd6aa01
Karan <periyapatnakaran@gmail.com>
2026-03-19 06:58:13 +0530

    updated link in faq remove agent word

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ const CONFIG = {
     JOB_URL: "hire-job.html",
     AGENT_URL: "agent-registration.html",
     SUPPORT_EMAIL: "support@digilaboursolutions.com",
-    SUPPORT_CONTACT_NO: "9986909171"
+    SUPPORT_CONTACT_NO: "9986909171",
+    JOB_LINK: "hire-job.html",
+    PARTNER_LINK: "agent-registration.html"
 };
